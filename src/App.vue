3f3479389da973646f2a833e15d48db5f61973ff
Leonardohas <leonardohas83@gmail.com>
2023-09-13 14:46:59 -0300

    First practices done

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-main>
+      <!-- <FirstComponent />
+      <LifeCycle /> 
+      <Info />  -->
+       <!-- <AvatarInfos /> -->
+       <realTImeCalculator />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import FirstComponent from './components/FirstComponent.vue'
+import LifeCycle from './components/LifeCycle.vue'
+import Info from './components/Info.vue'
+import AvatarInfos from './components/AvatarInfos.vue'
+import realTImeCalculator from './components/realTImeCalculator.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    FirstComponent,
+    LifeCycle,
+    Info,
+    AvatarInfos,
+    realTImeCalculator,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h1>Treino para o projeto mimo art</h1>
+      <p>Meu nome é {{ personalInfo.name }} e tenho {{ personalInfo.age }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "FirstComponent",
+  data() {
+    return {
+      personalInfo: {
+        name: "Leonardo Henrique Alves Silva",
+        age: "26 anos",
+      }
+    }
+  },
+}
+</script>
